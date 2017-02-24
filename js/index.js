f56604af102b745df4f8799d3971cfa574d40a0d
Dmytro Kachura <dkachura1999@gmail.com>
2017-02-24 04:49:04 +0000

    a

--- js/index.js
+++ js/index.js
@@ -8,13 +8,13 @@ function calc() {
  }
   if (D < 0) {
     return ('No results')
-  } else {
-  return (x1, x2)
+  } if (D > 0) {
+  return [x1, x2];
   }
 }
-var  D = b*b - 4 * a * c;
+var  D = (b * b) - (4 * a * c);
 var korD = Math.sqrt(D);
-var x = -b / 2 * a;
-  var x1 = (-b + korD) / 2 * a;
-  var x2 = (-b - korD) / 2 * a;
-document.write(x)
+var x = -b / (2 * a);
+  var x1 = (-b + korD) / (2 * a);
+  var x2 = (-b - korD) / (2 * a);
+document.write(calc());
