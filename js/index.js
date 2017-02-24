f5cd5deda7e787d3592a6a5cbba1ea0bd756b60e
Dmytro Kachura <dkachura1999@gmail.com>
2017-02-24 15:45:01 +0000

    a

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 var a = prompt ('Enter a');
 var b = prompt ('Enter b');
 var c = prompt ('Enter c');
-calc();
+var  D = (b * b) - (4 * a * c);
+var korD = Math.sqrt(D);
+var x = -b / (2 * a);
+  var x1 = (-b + korD) / (2 * a);
+  var x2 = (-b - korD) / (2 * a);
 function calc() {
  if (D == 0) {
   return (x)
@@ -12,9 +16,4 @@ function calc() {
   return [x1, x2];
   }
 }
-var  D = (b * b) - (4 * a * c);
-var korD = Math.sqrt(D);
-var x = -b / (2 * a);
-  var x1 = (-b + korD) / (2 * a);
-  var x2 = (-b - korD) / (2 * a);
 document.write(calc());
